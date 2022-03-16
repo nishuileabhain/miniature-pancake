5321951aa37f7cba2c26a1f32e6c2bc181f05528
nishuileabhain <nishuileabhain@gmail.com>
2022-03-16 22:44:38 +0000

    create models and migrate them to the db

--- speeches/admin.py
+++ speeches/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Speech, Speechlength
 
-# Register your models here.
+admin.site.register(Speech)
+admin.site.register(Speechlength)
--- speeches/models.py
+++ speeches/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Speechlength(models.Model):
+    title = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.title
+
+class Speech(models.Model):
+    title = models.CharField(max_length=100)
+    Speechlength = models.ForeignKey('Speechlength', null=False, blank=False, on_delete=models.CASCADE)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    # image_url = models.URLField(max_length=1024, null=True, blank=True)
+    # image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
