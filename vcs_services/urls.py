4c0a9865376bed02e67040bf05cf6a04139a6aac
nishuileabhain <nishuileabhain@gmail.com>
2022-03-16 19:28:27 +0000

    create homepage header and link css

--- vcs_services/urls.py
+++ vcs_services/urls.py
@@ -15,6 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from speeches import views
 
 urlpatterns = [
@@ -23,4 +26,4 @@ urlpatterns = [
     path('', include('home.urls')),
     path('speeches', views.home, name='home'),
     path('order', views.order, name='order'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
