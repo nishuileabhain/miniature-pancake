b9b27829ad271c8b4de5bb70cb220f8c2ae80633
nishuileabhain <nishuileabhain@gmail.com>
2022-03-17 19:27:40 +0000

    add speech details page

--- speeches/urls.py
+++ speeches/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_speeches, name='speeches')
+    path('', views.all_speeches, name='speeches'),
+    path('<speech_id>', views.speech_detail, name='speech_detail')
 ]
--- speeches/views.py
+++ speeches/views.py
@@ -1,14 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Speech
 
 def all_speeches(request):
     """ this shows every speech in the database """
     speeches = Speech.objects.all()
     context = {
-        'speeches' : speeches,
+        'speeches': speeches,
     }
 
     return render(request, 'speeches/speeches.html', context)
+
+
+def speech_detail(request, speech_id):
+    """ this shows the details of a particular speech """
+    speech = get_object_or_404(Speech, pk=speech_id)
+    context = {
+        'speech': speech,
+    }
+
+    return render(request, 'speeches/speech_detail.html', context)
+
 
 # def home(request):
 #     return render(request, 'speeches/home.html')
