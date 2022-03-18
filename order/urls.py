52d92cd8e89937e1ba308066b5f25c5479c6b977
nishuileabhain <nishuileabhain@gmail.com>
2022-03-18 07:02:34 +0000

    add to order in speech detail page

--- order/urls.py
+++ order/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.view_order, name='view_order')
+    path('', views.view_order, name='view_order'),
+    path('add/<item_id>/', views.add_to_order, name='add_to_order'),
+
 ]
--- order/views.py
+++ order/views.py
@@ -3,6 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 
 def view_order(request):
-    """ A view that renders the bag contents page """
+    """ A view that renders the order contents page """
 
     return render(request, 'order/order.html')
+
+def add_to_order(request, item_id):
+    """ Add a quantity of the speech to the order """
+
+    quantity = int(request.POST.get('quantity'))
+    redirect_url = request.POST.get('redirect_url')
+    order = request.session.get('order', {})
+
+    if item_id in list(order.keys()):
+        order[item_id] += quantity
+    else:
+        order[item_id] = quantity
+
+    request.session['order'] = order
+    print(request.session['order'])
+    return redirect(redirect_url)
